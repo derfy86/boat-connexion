f17287752746be406a5759fd6983da33b428b807
fred <bourigeaudfrederic@yahoo.fr>
2021-05-30 23:56:19 +0200

    socket.io connected

--- back/index.js
+++ back/index.js
@@ -4,45 +4,81 @@
  */
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const socketIo = require("socket.io");
+const http = require('http');
 
 /*
  * Vars
  */
 const app = express();
+const server = http.createServer(app);
+console.log(`server`, server)
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:8080",
+    methods: ["GET", "POST"]
+  }
+});
 const port = 3000;
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const redis = require("socket.io-redis");
+const corsOptions = {
+  origin: 'http://localhost:8080',
+  optionsSuccessStatus: 200 
+}
 
 /*
  * Express
  */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+app.use((request, response, next) => {
+  response.header('Access-Control-Allow-Origin', '*');
+  // response.header('Access-Control-Allow-Credentials', true);
+  response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  response.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
 });
+app.use(cors('*'));
+
 /**
- * Socket
+ * Socket / send infos to the front
  */
-var net = require('net');
 
-var client = new net.Socket();
-client.connect(10111, '127.0.0.1', function() {
-console.log('Connected');
+io.on("connection", (socket) => {
+  console.log('>> socket.io - connected');
+  socket.on('messages', function(data) {
+    console.log(data);
+ });
 });
 
-client.on('error', function(err) {
-console.error('Connection error: ' + err);
-console.error(new Error().stack);
-});
-
-client.on('data', function(data) {
-console.log('Received: ' + data);
+io.on("connection", function(socket) {
+  console.log(socket.conn.remoteAddress);
 });
 
 
-http.listen(process.env.PORT || 3000, function() {
+/**
+ * get the infos from multiplexer
+ */
+// const net = require('net');
+
+// const client = new net.Socket();
+// client.connect(3000, '127.0.0.1', function() {
+//   console.log('Connected');
+// });
+
+// client.on('error', function(err) {
+//   console.error('Connection error: ' + err);
+//   console.error(new Error().stack);
+// });
+
+// client.on('data', function(data) {
+//   console.log('Received: ' + data);
+// });
+
+
+/**
+ * server listener
+ */
+server.listen(process.env.PORT || 3000, '127.0.0.1' , function() {
   console.log('App listening on port' + ' ' + port)
 });
--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -1,18 +1,39 @@
 // == Import npm
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { io } from "socket.io-client";
 
 // == Import
 import './style.scss';
 
 import MessageData from '../../components/MessageData';
 
+let socket;
 // == Composant
-const App = () => (
+const App = () => {
+  
+  const endPoint = 'http://localhost:3000'
+
+  const [ data, setData ] = useState([])
+
+  useEffect(() => {
+    socket = io(endPoint);
+
+    socket.on("connect", () => {
+      console.log('>> socket.io - front - connected');
+    });
+
+    socket.on('send_message', (message) => {
+      console.log('un message', message);
+      setData([...data, message])
+    });
+  }, []);
+
+  return (
   <div className="app">
     <h1>my dashbord</h1>
     <MessageData />
   </div>
-);
+);}
 
 // == Export
 export default App;
