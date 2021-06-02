e644568e480a5e618abe47e8a96a37cc5607e05c
fred <bourigeaudfrederic@yahoo.fr>
2021-06-02 15:40:49 +0200

    parser done

--- back/index.js
+++ back/index.js
@@ -8,6 +8,10 @@ const cors = require('cors');
 const socketIo = require("socket.io");
 const http = require('http');
 const net = require('net');
+const dateFormat = require("dateformat");
+const convert = require('geo-coordinates-parser');
+const formatcoords = require('formatcoords');
+const GeoJSON = require('geojson');
 
 /*
  * Vars
@@ -43,7 +47,6 @@ app.use(cors('*'));
 /**
  * get the infos from multiplexer
  */
-
 const client = new net.Socket();
 
 client.connect(10111, '127.0.0.1', function() {
@@ -55,35 +58,90 @@ client.on('error', function(err) {
   console.error(new Error().stack);
 });
 
-// let myData = [];
+let myData = [];
 client.on('data', function(data) {
-  console.log('Received: ' + data);
-  // myData.push(data);
-  client.emit('test', {msg : data})
+  // console.log('Received: ' + data);
+  const raw = data.toString().split(','); //'$GPRMC,100803.76,A,3540.8323100,N,13946.1512988,E,16.3,25.3,020621,173.1,W,A,S*23'
+
+  let converted; // convert to decimal
+  let format; // beautify the coords
+  let geoJsonIo
+  try {
+    const convertFully = raw[3] + raw[4] + ' ' + raw[5] + raw[6];
+    converted = convert(convertFully);
+    format = formatcoords(converted.decimalLatitude, converted.decimalLongitude).format();
+
+    const geoLoc = [
+      { name: 'Location A', category: 'home', street: 'Market', lat: converted.decimalLatitude, lng: converted.decimalLongitude },
+    ];
+    const geo = GeoJSON.parse(geoLoc, {Point: ['lat', 'lng']});
+    geoJsonIo = JSON.stringify(geo); // to check on map by geojson.io
+  }
+  catch {
+    console.log('bad getaway')
+  }
+
+  let mode; 
+  switch(raw[12]){
+    case 'A':
+      mode = 'autonomous'
+      break;
+    case 'D':
+      mode = 'differential'
+      break;
+    case 'E':
+      mode = 'estimated'
+      break;
+    case 'M':
+      mode = 'manual input'
+      break;
+    case 'S':
+      mode = 'simulated'
+      break;
+    case 'N':
+      mode = 'data not valid'
+      break;
+    case 'P':
+      mode = 'precise'
+      break;
+    default:
+      mode = 'none'
+  }
+
+  const magneticVariation = raw[10] && raw[11] !== undefined ? raw[10] + ',' + raw[11] : 'none';
+
+  const dataParsed = {
+    type: raw[0].substring(3,7),
+    time: raw[1].substring(0,2) + ':' + raw[1].substring(2,4) + ':' + raw[1].substring(4,6),
+    status: raw[2] === 'A' ? 'active' : 'void',
+    coords: format,
+    geoloc: geoJsonIo,
+    speed: { knots: Number(raw[7]), Kmh: Number((raw[7]*1.852).toFixed(2)) },
+    track: raw[8],
+    date: raw[9].substring(0,2) + '.' + raw[9].substring(2,4) + '.' + raw[9].substring(4,6),
+    magneticVariation: magneticVariation,
+    mode: mode,
+    ChecksumData: raw[13].substring(0,4),
+  }
+  console.log(`dataParsed`, dataParsed)
+  myData.push(dataParsed);
+  // client.emit('test', {msg : data})
 });
-
-
-
-// app.get('/', (request, response) => {
-//   console.log(`myData`, myData[myData.length - 1])
-//   response.send('VMS server' + myData[myData.length - 1]);
-// });
 
 /**
  * Socket / send infos to the front
  */
+io.on("connection", (socket) => {
+  console.log('>> socket.io - connected');
+  // console.log(`myData`, myData[myData.length - 1])
+  socket.on('data', (message) => {
+    io.emit('data', message);
+  });
+});
 
-// io.on("connection", (socket) => {
-//   console.log('>> socket.io - connected');
-//   console.log(`myData`, myData[myData.length - 1])
-//   socket.on('data', (message) => {
-//     io.emit('data', message);
-//   });
-// });
-
-// io.on("connection", function(socket) {
-//   console.log(socket.conn.remoteAddress);
-// });
+io.on("connection", function(socket) {
+  console.log(socket.conn.remoteAddress);
+});
 
 /**
  * server listener
