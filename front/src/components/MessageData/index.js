d0bed79cee0cc7ac7b25007c616cd9eeff56acca
fred <bourigeaudfrederic@yahoo.fr>
2021-06-05 13:12:26 +0200

    front in evo

--- front/src/components/MessageData/index.js
+++ front/src/components/MessageData/index.js
@@ -1,15 +1,50 @@
 // == Import npm
-import React from 'react';
+import React, {useEffect} from 'react';
+import axios from 'axios'
 
 // == Import
 import './style.scss';
+import ReactSpeedometer from "react-d3-speedometer"
 
 // == Composant
 const MessageData = ({data}) => {
-  console.log(`data`, data)
+  console.log(`data`, data);
+  let speed; 
+  if(data.speed){
+    speed = data.speed.knots;
+  }
+  useEffect(()=>{
+  axios.get('https://maps.googleapis.com/maps/api/staticmap?center=40.714%2c%20-73.998&zoom=12&size=400x400&key=AIzaSyAZwZdeAzzPoFFWFQT1VcRErvxdODQlHFA')
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .then(function () {
+  });
+  })
   return(
   <>
-    <h1>data = {data.coords}</h1>
+    <p>time = {data.time}</p>
+    <p>date = {data.date}</p>
+    <p>coords = {data.coords}</p>
+    <p>mode = {data.mode}</p>
+    <div style={{
+      width: "500px",
+      height: "300px",
+      background: "#EFEFEF"
+    }}>
+  <ReactSpeedometer
+    fluidWidth={true}
+    minValue={0}
+    maxValue={30}
+    value={speed}
+    currentValueText={'knots'}
+    maxSegmentLabels={5}
+    segments={1000}
+  />
+  </div>
   </>
 );}
 
